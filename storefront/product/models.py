b9079736093cf976124cfde0e72f827122b82a3f
Alvaro Santana <alvaro.santana23@gmail.com>
2021-09-09 00:09:29 -0500

    Cleaning landing page, first object added

---
+++ storefront/product/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+#Modelo base, nombre es el nombre completo y slug es la version corta
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/{self.slug}'
