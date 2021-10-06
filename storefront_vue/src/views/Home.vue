0572dd83514b4c5c53091f99d6fc977ff03dda7d
Sergio Eduardo Vega Guzmán <48001421+classic-mathematician@users.noreply.github.com>
2021-10-06 03:56:41 -0500

    cambios

--- storefront_vue/src/views/Home.vue
+++ storefront_vue/src/views/Home.vue
@@ -11,7 +11,7 @@
       <div class="column is-12">
         <h2 class="is-size-2 has-text-centered">Latest products</h2>
       </div>
-      <ProductBox 
+      <ProductBox
         v-for="product in latestProducts"
         v-bind:key="product.id"
         v-bind:product="product" />
@@ -21,7 +21,7 @@
           </figure>
           <h3 class="is-size-4">{{ product.name }} </h3>
           <p class="is-size-6 has-text-grey">${{product.price}}</p>
-          View Details
+          <router-link v-bind:to="product.get_absolute_url" class = "button is-dark mt-4">View details</router-link>
         </div>
 
     </div>
